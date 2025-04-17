3309bf9e689ccaf20ac3324fba920d7b23a8f6ae
Yevhenii0536 <eugeniy0536@gmail.com>
2025-04-17 18:24:51 +0300

    added dotenv

--- security.py
+++ security.py
@@ -1,12 +1,17 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = "'njctrhtnysqrk.xRjnjhsqYbrnjYtPyftn"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
